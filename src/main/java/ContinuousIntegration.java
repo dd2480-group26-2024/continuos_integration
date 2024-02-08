999bbe42e8254d3b982c8eccd98ba0440ce7eed1
antonbol <antonbol@kth.se>
2024-02-08 09:45:49 +0100

    fix: changed port to 8026

--- src/main/java/ContinuousIntegration.java
+++ src/main/java/ContinuousIntegration.java
@@ -70,7 +70,7 @@ public class ContinuousIntegration extends AbstractHandler
     // used to start the CI server in command line
     public static void main(String[] args) throws Exception
     {
-        Server server = new Server(8080);
+        Server server = new Server(8026);
         server.setHandler(new ContinuousIntegration()); 
         server.start();
         server.join();
