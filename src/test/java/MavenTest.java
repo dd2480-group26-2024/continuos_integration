68d6a302ed07173a2bf84713b9b569bdab7da19e
rgrsst <rgrasset@kth.se>
2024-02-08 09:01:33 +0100

    feat: Added Maven build support | refactor: modified the project structure accordingly

---
+++ src/test/java/MavenTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MavenTest {
+
+    @Test
+    public void trueTest() {
+        assertEquals(1, 1);
+    }
+
+}
