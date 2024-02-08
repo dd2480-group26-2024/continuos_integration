a302902cf2eefa1d87e0f1427f1091d01e2f692e
antonbol <antonbol@kth.se>
2024-02-08 10:18:29 +0100

    feat: added test for checkout to ensure we are on right commit id

--- src/test/java/MavenTest.java
+++ src/test/java/MavenTest.java
@@ -1,7 +1,11 @@
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -17,7 +21,7 @@ public class MavenTest {
         // Arrange
         ContinuousIntegration ci = new ContinuousIntegration();
         String repoUrl = "https://github.com/dd2480-group26-2024/decide"; // replace with your repo URL
-        String commitId = "6cdd0d9"; // replace with a commit id to test
+        String commitId = "6cdd0d9b6d2d60956e9bb4750d4f1431219a5616"; // replace with a commit id to test
         String directoryPath = tempDir.toString(); // Use JUnit's @TempDir feature to create a temporary directory
 
         try {
@@ -27,6 +31,15 @@ public class MavenTest {
             // Assert
             Path repoPath = tempDir.resolve(".git"); // Path to the .git directory
             assertTrue(Files.exists(repoPath), "Repository .git directory should exist after cloning");
+
+            FileRepositoryBuilder repositoryBuilder = new FileRepositoryBuilder();
+            try (Repository repository = repositoryBuilder.setGitDir(repoPath.toFile()).readEnvironment().findGitDir().build();
+                RevWalk revWalk = new RevWalk(repository)) {
+                ObjectId lastCommitId = repository.resolve(commitId);
+                RevCommit revCommit = revWalk.parseCommit(lastCommitId);
+                System.out.println("");
+                assertEquals(commitId, revCommit.getId().getName(), "The checked out commit ID should match the requested commit ID.");
+            }
 
             //JUnit cleans up the tempdir automatically!
         } catch (Exception e) {
