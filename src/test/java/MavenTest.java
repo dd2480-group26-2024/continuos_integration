3a2e57cc9958eda93ee075efe86d0707cae7f2a1
rgrsst <rgrasset@kth.se>
2024-02-14 13:34:59 +0100

    test CI server - test should pass

--- src/test/java/MavenTest.java
+++ src/test/java/MavenTest.java
@@ -155,7 +155,7 @@ public class MavenTest {
 		requestData.put("email", "robin.yurt@hotmail.com"); // Replace with the recipient's email address
 
 		// Call the method and assert the result
-		assertFalse(ci.sendEmailNotification(requestData, true));
+		assertTrue(ci.sendEmailNotification(requestData, true));
 	}
 	
 	@Test
